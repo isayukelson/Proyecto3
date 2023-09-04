ea4ec8fc601bdb2fc58873f32db0eb3fac64a9b0
victoriamoises03 <vmoises@udesa.edu.ar>
2023-09-04 10:56:27 -0300

    header y footer

---
+++ src/componentes/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Header() {
+    return(
+        <nav>
+            <a href="/HomePage">Home</a>
+            <a href="/Favoritos">Favoritos</a>
+        </nav>
+    )
+}
